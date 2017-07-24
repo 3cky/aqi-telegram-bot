d9a3960601319f8b0859205f52a79f685e6f414f
Victor Antonovich <v.antonovich@gmail.com>
2017-07-24 16:12:27 +0300

    Add refresh button to /aqi and /pm command results

--- telegram/bot.py
+++ telegram/bot.py
@@ -43,12 +43,7 @@ class Bot(service.Service, BotPlugin):
                  u'Please use /help for list of available commands.') % {'cmd': cmd}
 
     def on_command_start(self, _args, cmd_msg):
-        m = sendMessage()
-        m.chat_id = cmd_msg.chat.id
-        m.text = _(u"Hello, I'm *AQI monitor bot*.\nFor help, please use /help command.")
-        m.parse_mode = 'Markdown'
-        m.reply_markup = self.inline_keyboard()
-        return m
+        return _(u"Hello, I'm *AQI monitor bot*.\nFor help, please use /help command.")
 
     def on_command_help(self, _args, _msg):
         return _(u'*Available commands:*\n\n' +
@@ -56,22 +51,42 @@ class Bot(service.Service, BotPlugin):
                  u'/pm - show current PM values\n' +
                  u'/sensor\_info - show PM sensor information')
 
-    def on_command_aqi(self, _args, _msg):
+    def cmd_response(self, resp, chat_id, text, cmd):
+        m = resp()
+        m.chat_id = chat_id
+        m.text = text
+        m.parse_mode = 'Markdown'
+        m.reply_markup = self.cmd_refresh_button(cmd)
+        return m
+
+    def cmd_refresh_button(self, cmd):
+        keyboard = InlineKeyboardMarkup()
+        buttons = []
+        b = InlineKeyboardButton()
+        b.text = _(u'Refresh')
+        b.callback_data = cmd
+        buttons.append(b)
+        keyboard.inline_keyboard = [buttons]
+        return keyboard
+
+    def on_command_aqi(self, _args, msg):
         data_timestamp = self.aqi_monitor.data_timestamp
         if data_timestamp is None:
             return _(u'No data from PM sensor obtained yet.')
         aqi = self.aqi_monitor.current_aqi()
         rtime = self.format_timedelta(data_timestamp)
-        return _(u'AQI: *%(aqi)s* (updated %(rtime)s ago)') % {'aqi': aqi, 'rtime': rtime}
+        text = _(u'AQI: *%(aqi)s* (updated %(rtime)s ago)') % {'aqi': aqi, 'rtime': rtime}
+        return self.cmd_response(sendMessage, msg.chat.id, text, 'aqi')
 
-    def on_command_pm(self, _args, _msg):
+    def on_command_pm(self, _args, msg):
         data_timestamp = self.aqi_monitor.data_timestamp
         if data_timestamp is None:
             return _(u'No data from PM sensor obtained yet.')
         pm_25, pm_10 = self.aqi_monitor.current_pm()
         rtime = self.format_timedelta(data_timestamp)
-        return _(u'PM2.5: *%(pm_25)s* μg/m^3\nPM10: *%(pm_10)s* μg/m^3\n' +
+        text = _(u'PM2.5: *%(pm_25)s* μg/m^3\nPM10: *%(pm_10)s* μg/m^3\n' +
                  u'(updated %(rtime)s ago)') % {'pm_25': pm_25, 'pm_10': pm_10, 'rtime': rtime}
+        return self.cmd_response(sendMessage, msg.chat.id, text, 'pm')
 
     @defer.inlineCallbacks
     def on_command_sensor_info(self, _args, _msg):
@@ -86,29 +101,14 @@ class Bot(service.Service, BotPlugin):
         yield self.send_method(m)
 
         # update message with command result (if text updated)
-        text = yield self.on_command(callback_query.data)
+        cmd = callback_query.data
+        msg = callback_query.message
+        cmd_resp = yield self.on_command(cmd, cmd_msg=msg)
+        text = cmd_resp.text
         plain_text = BeautifulSoup(markdown2.markdown(text), "html.parser").get_text().strip()
-        if plain_text != callback_query.message.text:
-            m = editMessageText()
-            m.chat_id = callback_query.message.chat.id
-            m.message_id = callback_query.message.message_id
-            m.text = text
-            m.parse_mode = 'Markdown'
-            m.reply_markup = self.inline_keyboard()
+        if plain_text != msg.text:
+            m = self.cmd_response(editMessageText, msg.chat.id, text, cmd)
+            m.message_id = msg.message_id
             yield self.send_method(m)
 
         defer.returnValue(True)
-
-    def inline_keyboard(self):
-        keyboard = InlineKeyboardMarkup()
-        buttons = []
-        b = InlineKeyboardButton()
-        b.text = 'AQI'
-        b.callback_data = 'aqi'
-        buttons.append(b)
-        b = InlineKeyboardButton()
-        b.text = 'PM'
-        b.callback_data = 'pm'
-        buttons.append(b)
-        keyboard.inline_keyboard = [buttons]
-        return keyboard
